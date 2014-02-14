d8c6abc99b5ea57e91c3d4d1dbfa8207c61a0700
Sapna Patel <sapnapatel@rescomp-13-203815.stanford.edu>
2014-02-14 14:54:10 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,39 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	console.log("/project/" + idNumber);
+	$.get("/project/" + idNumber, addProject);
+}
+
+function addProject(result) {
+	console.log(result);
+	console.log("projectHTML: ");
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>' + '<p>' + result['summary'] + '</p>'; 
+   	console.log(projectHTML);
+
+   	var projectID = "project" + result['id'];
+   	console.log(projectID);
+
+   	console.log($('[id^="'+projectID+'"] .details').length);
+   	$('[id^="'+projectID+'"] .details').html(projectHTML);
+
+}
+
+function addColor(result) {
+	console.log("color result: " + result);
+
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);	
 }
 
 /*
@@ -35,4 +68,6 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", addColor);
 }
